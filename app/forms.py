24b7eec91bf416685df5dcde933dd0be99fdf533
Alberto Casado Medina <albertocasadom@gmail.com>
2020-04-04 15:32:02 +0200

    Adding new parameters to the register form

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,31 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo, Length
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, RadioField
+from wtforms.validators import DataRequired, Email, EqualTo, Length, NumberRange, InputRequired
+from wtforms.widgets import html5, html_params, HTMLString
 
+class ButtonWidget(object):
+    """
+    Renders a multi-line text area.
+    `rows` and `cols` ought to be passed as keyword args when rendering.
+    """
+    input_type = 'button'
+
+    html_params = staticmethod(html_params)
+
+    def __call__(self, field, **kwargs):
+        kwargs.setdefault('id', field.id)
+        kwargs.setdefault('type', self.input_type)
+        if 'value' not in kwargs:
+            kwargs['value'] = field._value()
+
+        return HTMLString('<button {params}>{label}</button>'.format(
+            params=self.html_params(name=field.name, **kwargs),
+            label=field.label.text)
+        )
+
+
+class ButtonField(StringField):
+    widget = ButtonWidget()
 
 class LoginForm(FlaskForm):
     """User Login Form."""
@@ -27,4 +51,14 @@ class RegisterForm(FlaskForm):
         EqualTo('password_confirm', message='Password must match')
     ])
     password_confirm = PasswordField('Confirm your Password')
+
+    donate = ButtonField('Donar')
+    supply = ButtonField('Transportar')
+    apply = ButtonField('Solicitar')
+
+    address = StringField('Direccion', validators=[ DataRequired('Por favor, introduzca una direccion postal valida')])
+
+    usertype_radiobutton = RadioField('Seleccione su rol', choices=[('Donante','Donante'),('Repartidor','Repartidor'),('Solicitante','Solicitante')])
+
+    
     submit = SubmitField('Register')
